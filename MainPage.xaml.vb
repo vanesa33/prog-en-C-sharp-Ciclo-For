c4e3e7143845513b6185057d06060e265d02129d
vanesa33 <57409926+vanesa33@users.noreply.github.com>
2021-02-04 17:15:34 -0300

    Delete MainPage.xaml.vb

--- MainPage.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿' La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
-
-''' <summary>
-''' Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-''' </summary>
-Public NotInheritable Class MainPage
-    Inherits Page
-
-End Class
