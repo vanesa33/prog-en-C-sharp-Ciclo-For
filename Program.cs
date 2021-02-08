59dc3f643bb3e41e1dc50a1c2f87ddcb9afcca96
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 14:59:09 -0300

    carpeta ciclo for archivo.csproj y .cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclo_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre =("learnwtutorials");
+
+            foreach (char letra in nombre)
+            {
+                Console.Write(" " + letra + " ");
+            
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
